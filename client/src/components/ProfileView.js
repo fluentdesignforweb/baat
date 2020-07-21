c82a1e09c043e8714f8227354d6554cfc3760a2e
Shivendra Saurav <shivendrasaurav@gmail.com>
2020-07-21 12:49:44 +0530

    Routes Setup + Components Predefined

---
+++ client/src/components/ProfileView.js
@@ -0,0 +1,16 @@
+import React, { Fragment, useState, useEffect } from "react";
+import "./extrastyles.css";
+
+const ChatWindow = () => {
+
+    return(
+        <Fragment>
+
+            //HTML COMES HERE
+
+        </Fragment>
+    );
+
+}
+
+export default ChatWindow;
